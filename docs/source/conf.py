ad1a37b1b4ba55406e9af9b9ac41124b25111ab8
spaceartisan <spaceartisan@gmail.com>
2023-01-25 02:56:28 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ release = 'Jan 22, 2023'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon','sphinx.ext.autosummary','sphinx.ext.viewcode','sphinx.ext.todo','sphinx.ext.autosectionlabel']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon','sphinx.ext.autosummary','sphinx.ext.viewcode','sphinx.ext.todo','sphinx.ext.autosectionlabel','autodocsumm']
 autosummary_generate = True
 todo_include_todos = True
 # Add any paths that contain templates here, relative to this directory.
